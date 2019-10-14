b09fecfab787512ac738a2f3436cb1b2e2471061
zxhbloomer <zxhbloomer@gmail.com>
2019-10-14 16:15:09 +0800

    ..

---
+++ perfect-task/src/main/java/com/perfect/task/starter/SchedulerTaskServerStart.java
@@ -0,0 +1,32 @@
+package com.perfect.task.starter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author zxh
+ */
+@SpringBootApplication(
+    exclude = { DataSourceAutoConfiguration.class },
+    scanBasePackages = {
+            "com.perfect.framework",
+            "com.perfect.*",
+            "com.perfect.redis",
+            "com.perfect.quartz.config"
+        })
+@EnableTransactionManagement
+@EntityScan(basePackages = {"com.perfect.*"})
+@Slf4j
+@EnableCaching
+public class SchedulerTaskServerStart {
+    public static void main(String[] args) {
+        log.info("-----------------------启动开始-------------------------");
+        SpringApplication.run(SchedulerTaskServerStart.class, args);
+        log.info("-----------------------启动完毕-------------------------");
+    }
+}
