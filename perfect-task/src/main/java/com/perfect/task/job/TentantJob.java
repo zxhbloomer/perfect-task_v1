ee324beba90adc7e36742e49dca152fa9625bc3e
zxhbloomer@gmail.com <Lyyzxh030324>
2019-10-22 23:08:14 +0800

    ..

---
+++ perfect-task/src/main/java/com/perfect/task/job/TentantJob.java
@@ -0,0 +1,31 @@
+package com.perfect.task.job;
+
+import com.perfect.bean.entity.quartz.SJobEntity;
+import com.perfect.common.exception.job.TaskException;
+import com.perfect.core.service.quartz.ISJobService;
+import com.perfect.quartz.util.AbstractQuartzJob;
+import com.perfect.quartz.util.ScheduleUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+/**
+ * 处理租户定时任务
+ *
+ * @author zxh
+ * @date 2019年 10月20日 17:12:23
+ */
+@Component
+@Slf4j
+public class TentantJob extends AbstractQuartzJob {
+
+    @Autowired
+    private ISJobService service;
+
+    @Override
+    protected void doExecute(JobExecutionContext context, SJobEntity sysJob) throws Exception {
+        log.debug("test");
+    }
+}
--- perfect-task/src/main/java/com/perfect/task/mq/consumer/TentantConsumer.java
+++ perfect-task/src/main/java/com/perfect/task/mq/consumer/TentantConsumer.java
@@ -1,19 +1,22 @@
-package com.perfect.task.job.consumer;
+package com.perfect.task.mq.consumer;
 
 import com.perfect.bean.entity.quartz.SJobEntity;
 import com.perfect.bean.pojo.mqsender.MqSenderPojo;
 import com.perfect.common.exception.job.TaskException;
+import com.perfect.core.service.quartz.ISJobService;
 import com.perfect.framework.utils.mq.MessageUtil;
 import com.perfect.mq.rabbitmq.mqenum.MQEnum;
-import com.perfect.task.job.execute.TentantJobExecute;
+import com.perfect.quartz.util.ScheduleUtils;
+import com.perfect.task.job.TentantJob;
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
+import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.*;
-import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -34,7 +37,11 @@ import java.util.Map;
 public class TentantConsumer {
 
     @Autowired
-    TentantJobExecute tentantJobExecute;
+    private ISJobService service;
+
+    @Qualifier("perfectScheduler")
+    @Autowired
+    private Scheduler scheduler;
 
     /**
      * 配置监听的哪一个队列，同时在没有queue和exchange的情况下会去创建并建立绑定关系
@@ -59,7 +66,7 @@ public class TentantConsumer {
         /**
          * 执行job
          */
-        process((SJobEntity)messageContext);
+        executeTrigger((SJobEntity)messageContext);
 
         Long deliveryTag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
         String MESSAGE_ID = (String) headers.get(AmqpHeaders.MESSAGE_ID);
@@ -68,10 +75,46 @@ public class TentantConsumer {
     }
 
     /**
-     * 开始启用定时任务quartz
+     * 执行定时任务的触发器
      * @param job
+     * @throws TaskException
+     * @throws SchedulerException
      */
-    private void process(SJobEntity job) throws TaskException, SchedulerException {
-        tentantJobExecute.execute(job);
+    public void executeTrigger(SJobEntity job) throws TaskException, SchedulerException {
+        job.setJob_name(job.getJob_name()+"["+job.getJob_serial_id()+"]");
+        if(job.getId() == null){
+            // 先插入数据库，获取id
+            service.insert(job);
+        } else {
+            // 更新数据库，获取id
+            service.update(job);
+        }
+        if(job.getIs_cron()){
+            // 如果是cron表达式方式
+            executeCronTrigger(job);
+        } else {
+            // 调用的是simpletrigger方式
+            executeSimpleTrigger(job);
+        }
+    }
+
+    /**
+     * 执行一个用于触发的时间
+     * @param job
+     * @throws TaskException
+     * @throws SchedulerException
+     */
+    public void executeSimpleTrigger (SJobEntity job) throws TaskException, SchedulerException {
+        ScheduleUtils.createScheduleJobSimpleTrigger(scheduler, job, TentantJob.class);
+    }
+
+    /**
+     * 执行一个用于触发的时间
+     * @param job
+     * @throws TaskException
+     * @throws SchedulerException
+     */
+    public void executeCronTrigger (SJobEntity job) throws TaskException, SchedulerException {
+        ScheduleUtils.createScheduleJobCron(scheduler, job, TentantJob.class);
     }
 }
